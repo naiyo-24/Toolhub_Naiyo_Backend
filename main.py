39cac2db5e0560520a3125add09ddeea66bb6b68
sayar2121 <sayarpaul20004@gmail.com>
2026-09-07 19:27:20 +0530

    chore: restrict CORS allow_origins to specific authorized domains

--- main.py
+++ main.py
@@ -140,9 +140,16 @@ app = FastAPI(
     version="1.0.0"
 )
 
+origins = [
+    "http://localhost:5173", 
+    "https://toolhubutility.com", 
+    "https://www.toolhubutility.com",
+    "http://localhost:3000"
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
